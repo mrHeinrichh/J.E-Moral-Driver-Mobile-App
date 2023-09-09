bbdaf6e816d25b4ab261b54c66fa3462e2d033e8
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-09-09 23:33:22 +0800

    feat: :white_check_mark: Add modularized card for completed

--- lib/widgets/custom_cards.dart
+++ lib/widgets/custom_cards.dart
@@ -105,3 +105,97 @@ class CustomCard extends StatelessWidget {
     );
   }
 }
+
+class CompletedCard extends StatelessWidget {
+  final TextStyle customTextStyle;
+
+  CompletedCard({
+    required this.customTextStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(28, 10, 28, 10),
+      child: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(25, 25, 25, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Barangay:",
+                        style: customTextStyle,
+                      ),
+                      SizedBox(width: 150),
+                      Text(
+                        "Status:",
+                        style: customTextStyle,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "House#/Lot/Blk:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Address:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Booker Name:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Booker Contact:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Receiver Name:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Receiver Contact:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Payment Method:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Needs to be assembled:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Delivery Time:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Delivery Charge:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Product List:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Barangay:",
+                    style: customTextStyle,
+                  ),
+                  Text(
+                    "Total Price:",
+                    style: customTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
