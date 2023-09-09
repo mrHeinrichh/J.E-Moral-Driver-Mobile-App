1c4026392d095516d48e4152510f1aec483c1ff4
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-09-10 01:53:22 +0800

    feat: :white_check_mark: Add camera and image features

--- lib/widgets/custom_cards.dart
+++ lib/widgets/custom_cards.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'package:driver_app/widgets/card_button.dart';
 import 'package:flutter/material.dart';
 
@@ -189,6 +191,214 @@ class CompletedCard extends StatelessWidget {
                   Text(
                     "Total Price:",
                     style: customTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PickedUpCard extends StatefulWidget {
+  final TextStyle customTextStyle;
+  final String buttonText;
+  final VoidCallback onPressed;
+  final Color btncolor;
+
+  PickedUpCard({
+    required this.customTextStyle,
+    required this.buttonText,
+    required this.onPressed,
+    required this.btncolor,
+  });
+
+  @override
+  _PickedUpCardState createState() => _PickedUpCardState();
+}
+
+class _PickedUpCardState extends State<PickedUpCard> {
+  File? _image;
+  bool _imageCaptured = false;
+
+  Future<void> _getImageFromCamera() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+        _imageCaptured = true; // Image has been captured
+      });
+    }
+  }
+
+  void _deleteImage() {
+    setState(() {
+      _image = null;
+      _imageCaptured = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(28, 10, 28, 10),
+      child: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(25, 25, 25, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Barangay:",
+                        style: widget.customTextStyle,
+                      ),
+                      SizedBox(width: 150),
+                      Text(
+                        "Status:",
+                        style: widget.customTextStyle,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "House#/Lot/Blk:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Address:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Booker Name:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Booker Contact:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Receiver Name:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Receiver Contact:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Payment Method:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Needs to be assembled:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Delivery Time:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Delivery Charge:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Product List:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Barangay:",
+                    style: widget.customTextStyle,
+                  ),
+                  Text(
+                    "Total Price:",
+                    style: widget.customTextStyle,
+                  ),
+
+                  GestureDetector(
+                    onTap: () {
+                      if (_imageCaptured) {
+                        _deleteImage(); // Handle image deletion
+                      } else {
+                        _getImageFromCamera(); // Handle the click action
+                      }
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey, // Set the border color
+                          width: 2.0, // Set the border width
+                        ),
+                        borderRadius:
+                            BorderRadius.circular(8.0), // Set border radius
+                      ),
+                      child: Stack(
+                        children: [
+                          _image != null
+                              ? Image.file(
+                                  _image!,
+                                  width: double.infinity,
+                                  height: double.infinity,
+                                  fit: BoxFit.cover,
+                                )
+                              : const SizedBox.shrink(),
+                          Positioned(
+                            top: 5,
+                            right: 5,
+                            child: _imageCaptured
+                                ? IconButton(
+                                    onPressed: _deleteImage,
+                                    icon: const Icon(
+                                      Icons.close,
+                                      color: Colors.red,
+                                    ),
+                                  )
+                                : const SizedBox.shrink(),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Container(
+                              color: Colors.black54,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  _imageCaptured
+                                      ? _image != null
+                                          ? _image!.path.split('/').last
+                                          : 'No Image'
+                                      : '',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      width: double.infinity,
+                      height: 150.0, // Adjust the height as needed
+                    ),
+                  ),
+                  // Display the captured image
+                  // if (_image != null)
+                  //   Image.file(
+                  //     _image!,
+                  //     width: 100,
+                  //     height: 100,
+                  //   ),
+
+                  CardButton(
+                    text: widget.buttonText,
+                    onPressed: widget.onPressed,
+                    color: _imageCaptured
+                        ? const Color(0xFFBD2019) // Change to BD2019 color
+                        : widget.btncolor, // Use original color
                   ),
                 ],
               ),
